8749745d1b8ecddec6d2e10951e79c07c817be68
kostas <eoppep@eoppep.gr>
2022-12-01 14:22:04 +0200

    naiiiiiii

---
+++ src/client/index.js
@@ -0,0 +1,29 @@
+    
+    let subButton1 = document.querySelector("#submit1")
+    let subButton2 = document.querySelector("#submit2")
+    let textValue = document.querySelector("#field")
+    
+    subButton1.addEventListener("click", async (e)=>{
+        console.log(textValue.value)
+        const tvalue = textValue.value
+        const textInfo = {tvalue};
+        const headerInfoPost = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body:JSON.stringify(textInfo)
+        };
+        
+        
+        const response = await fetch('/b',headerInfoPost);
+
+    })
+
+    subButton2.addEventListener("click", async (e)=>{
+
+        const res = await fetch('/b')
+        const json = await res.json()
+        textValue.value = json.textInfo
+    })
+
--- src/server/server.js
+++ src/server/server.js
@@ -2,8 +2,26 @@ import express from "express";
 
 const testServer = express();
 
+
 testServer.use(express.static('../client'));
+testServer.use(express.json())
+let data;
+testServer.get('/g',(req,res)=>{
+    res.send('hhhh')
+})
 
+testServer.post('/b',(req,res)=>{
+    console.log(req)
+    console.log(req.body)
+     data = req.body.tvalue
+   res.end()
+})
 
+testServer.get('/b',(req,res) =>{
+    res.json({
+        textInfo: data
+    });
+    
+})
 
 testServer.listen(3000, ()=>console.log("testServer listens to port 3000"));
